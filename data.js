9682f9949a14cb223866d9e8ef3b02639bc90908
James Cuénod <j3frea@gmail.com>
2017-05-22 11:01:34 -0500

    Initial work on chartjs version (entry: index.html)

---
+++ data.js
@@ -0,0 +1,762 @@
+var dataFile = [
+//  {
+//    "date": "2012-01-01",
+//    "fed_unemployment": 8.3,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-02-01",
+//    "fed_unemployment": 8.3,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-03-01",
+//    "fed_unemployment": 8.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-04-01",
+//    "fed_unemployment": 8.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-05-01",
+//    "fed_unemployment": 8.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-06-01",
+//    "fed_unemployment": 8.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-07-01",
+//    "fed_unemployment": 8.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-08-01",
+//    "fed_unemployment": 8.1,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-09-01",
+//    "fed_unemployment": 7.8,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-10-01",
+//    "fed_unemployment": 7.8,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-10-19",
+//    "fed_unemployment": null,
+//    "fed_rate": 7.8,
+//    "trump_min": 15,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "Twitter",
+//    "trump_quote": "\"7.8% unemployment number is a complete fraud as evidenced by the jobless claims number released yesterday. Real unemployment is at least 15%\""
+//  },
+//  {
+//    "date": "2012-11-01",
+//    "fed_unemployment": 7.7,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2012-12-01",
+//    "fed_unemployment": 7.9,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-01-01",
+//    "fed_unemployment": 8,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-02-01",
+//    "fed_unemployment": 7.7,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-03-01",
+//    "fed_unemployment": 7.5,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-04-01",
+//    "fed_unemployment": 7.6,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-05-01",
+//    "fed_unemployment": 7.5,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-06-01",
+//    "fed_unemployment": 7.5,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-07-01",
+//    "fed_unemployment": 7.3,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-08-01",
+//    "fed_unemployment": 7.3,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-09-01",
+//    "fed_unemployment": 7.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-10-01",
+//    "fed_unemployment": 7.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-11-01",
+//    "fed_unemployment": 6.9,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2013-12-01",
+//    "fed_unemployment": 6.7,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-01-01",
+//    "fed_unemployment": 6.6,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-02-01",
+//    "fed_unemployment": 6.7,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-03-01",
+//    "fed_unemployment": 6.7,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-04-01",
+//    "fed_unemployment": 6.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-05-01",
+//    "fed_unemployment": 6.3,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-06-01",
+//    "fed_unemployment": 6.1,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-07-01",
+//    "fed_unemployment": 6.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-08-01",
+//    "fed_unemployment": 6.2,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-09-01",
+//    "fed_unemployment": 5.9,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-10-01",
+//    "fed_unemployment": 5.7,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-11-01",
+//    "fed_unemployment": 5.8,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2014-12-01",
+//    "fed_unemployment": 5.6,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2015-01-01",
+//    "fed_unemployment": 5.7,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2015-02-01",
+//    "fed_unemployment": 5.5,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2015-03-01",
+//    "fed_unemployment": 5.4,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2015-04-01",
+//    "fed_unemployment": 5.4,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+//  {
+//    "date": "2015-05-01",
+//    "fed_unemployment": 5.5,
+//    "fed_rate": null,
+//    "trump_min": null,
+//    "trump_unemployment": null,
+//    "trump_max": null,
+//    "trump_location": "",
+//    "trump_quote": ""
+//  },
+ {
+   "date": "2015-06-01",
+   "fed_unemployment": 5.3,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2015-06-16",
+   "fed_unemployment": null,
+   "fed_rate": 5.3,
+   "trump_min": 15,
+   "trump_unemployment": 19,
+   "trump_max": 22,
+   "trump_location": "Trump Tower",
+   "trump_quote": "\"Our real unemployment is anywhere from 18 to 20 percent. Don't believe the 5.6. Don't believe it.\""
+ },
+ {
+   "date": "2015-07-01",
+   "fed_unemployment": 5.2,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2015-07-08",
+   "fed_unemployment": null,
+   "fed_rate": 5.2,
+   "trump_min": 15,
+   "trump_unemployment": 16,
+   "trump_max": null,
+   "trump_location": "Family Leadership Summit",
+   "trump_quote": "\"It is not real. when you look at unemployment and it is 7.4%, 7.4%, it is probably anywhere from 15%-17%\""
+ },
+ {
+   "date": "2015-08-01",
+   "fed_unemployment": 5.1,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2015-08-11",
+   "fed_unemployment": null,
+   "fed_rate": 5.1,
+   "trump_min": null,
+   "trump_unemployment": 40,
+   "trump_max": 42,
+   "trump_location": "Hannity",
+   "trump_quote": "\"Then you hear there's a 5.4% unemployment. It's really - if you add it up, it's probably 40 percent if you think about it.\""
+ },
+ {
+   "date": "2015-08-18",
+   "fed_unemployment": null,
+   "fed_rate": 5.1,
+   "trump_min": null,
+   "trump_unemployment": 21,
+   "trump_max": 42,
+   "trump_location": "Time Magazine",
+   "trump_quote": "\"Don't forget in the meantime we have a real unemployment rate that's probably 21%. It's not 6. I's not 5.2 and 5.5. Our real unemployment rate-in fact, I saw a chart the other day, our real unemployment-because you have ninety million people that aren't working. Ninety-three million to be exact. If you start adding it up, our real unemployment rate is 42%\""
+ },
+ {
+   "date": "2015-08-30",
+   "fed_unemployment": null,
+   "fed_rate": 5.1,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": 42,
+   "trump_location": "Speech on CSPAN?",
+   "trump_quote": "\"They show those phony statistics where we are 5.4% unemployment. The real number, I saw a number that could be 42 percent, believe it or not.\""
+ },
+ {
+   "date": "2015-09-01",
+   "fed_unemployment": 5,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2015-09-28",
+   "fed_unemployment": null,
+   "fed_rate": 5,
+   "trump_min": null,
+   "trump_unemployment": 35,
+   "trump_max": 42,
+   "trump_location": "Press Conference (Where?)",
+   "trump_quote": "\"28, 29, as high as 35 [percent]. In fact, I even heard recently 42%\""
+ },
+ {
+   "date": "2015-09-28",
+   "fed_unemployment": null,
+   "fed_rate": 5,
+   "trump_min": 17,
+   "trump_unemployment": 20,
+   "trump_max": null,
+   "trump_location": "CSPAN??",
+   "trump_quote": "\"The unemployment rate is probably 20%\""
+ },
+ {
+   "date": "2015-09-29",
+   "fed_unemployment": null,
+   "fed_rate": 5,
+   "trump_min": null,
+   "trump_unemployment": 24,
+   "trump_max": 42,
+   "trump_location": "",
+   "trump_quote": "\"The number is not reflective. I have seen numbers of 24%. I saw a number of 42% unemployment. That number is so false.\""
+ },
+ {
+   "date": "2015-10-01",
+   "fed_unemployment": 5,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2015-10-09",
+   "fed_unemployment": null,
+   "fed_rate": 5,
+   "trump_min": null,
+   "trump_unemployment": 32,
+   "trump_max": 42,
+   "trump_location": "CSPAN??",
+   "trump_quote": "\"They say 5.3 percent employment. The number is probably 32%.\""
+ },
+ {
+   "date": "2015-11-01",
+   "fed_unemployment": 5,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2015-12-01",
+   "fed_unemployment": 5,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-01-01",
+   "fed_unemployment": 4.9,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-01-20",
+   "fed_unemployment": null,
+   "fed_rate": 4.9,
+   "trump_min": 18,
+   "trump_unemployment": 22,
+   "trump_max": 26,
+   "trump_location": "Liberty University",
+   "trump_quote": "\"They have this phony number, 5.2%. Everybody that quits looking for a job is considered statistically a person that has a job. It's a phony number. You probably - real numbers like 22, 23%\""
+ },
+ {
+   "date": "2016-02-01",
+   "fed_unemployment": 4.9,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-03-01",
+   "fed_unemployment": 5,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-04-01",
+   "fed_unemployment": 5,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-05-01",
+   "fed_unemployment": 4.7,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-05-27",
+   "fed_unemployment": null,
+   "fed_rate": 4.7,
+   "trump_min": 17,
+   "trump_unemployment": 20,
+   "trump_max": 23,
+   "trump_location": "San Diego",
+   "trump_quote": "\"The real [unemployment] number is 20%. The actual unemployment rate, that is, the rate that includes Americans who have given up hope of finding employment, is 23%.\""
+ },
+ {
+   "date": "2016-06-01",
+   "fed_unemployment": 4.9,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-07-01",
+   "fed_unemployment": 4.9,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-08-01",
+   "fed_unemployment": 4.9,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-09-01",
+   "fed_unemployment": 4.9,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-10-01",
+   "fed_unemployment": 4.8,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-11-01",
+   "fed_unemployment": 4.6,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2016-12-01",
+   "fed_unemployment": 4.7,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2017-01-01",
+   "fed_unemployment": 4.8,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2017-02-01",
+   "fed_unemployment": 4.7,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2017-03-01",
+   "fed_unemployment": 4.7,
+   "fed_rate": null,
+   "trump_min": null,
+   "trump_unemployment": null,
+   "trump_max": null,
+   "trump_location": "",
+   "trump_quote": ""
+ },
+ {
+   "date": "2017-03-01",
+   "fed_unemployment": null,
+   "fed_rate": 4.7,
+   "trump_min": 4.7,
+   "trump_unemployment": 4.7,
+   "trump_max": 4.7,
+   "trump_location": "Sean Spicer, White House Press briefing",
+   "trump_quote": "\"'They may have been phony in the past, but it's very real now,' Spicer said Friday from the White House podium, hours after the government announced 235,000 new jobs in February and a dip in the unemployment rate to 4.7% from 4.8%.\""
+ }
+]
